cf4e8043a8eea20d2a138f169b7618be6e8f8841
Jordan <Jordan.Yin04@gmail.com>
2024-11-19 11:54:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cors({
   origin: "https://screen-wise.vercel.app",
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Set-Cookie'],
   credentials: true
 }));
 
